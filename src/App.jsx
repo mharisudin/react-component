6525a13cc3bdce92ad873fb923ef706053b3639f
Muhammad Harisuddin <abukaukab.assalam@gmail.com>
2023-08-01 23:26:03 +0700

    install plugin clsx untuk membuat component intermediate

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
     IconLogin,
     IconRegistered,
 } from '@tabler/icons-react';
+import clsx from 'clsx';
 
 export default function App() {
     return (
@@ -45,7 +46,10 @@ function Button(props) {
     return (
         <button
             {...props}
-            className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+            className={clsx(
+                className,
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            )}
         >
             {text || children}
         </button>
